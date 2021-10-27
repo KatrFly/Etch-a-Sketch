34ecb905d8374db12fc6ff93bb29fb6c5fb82666
KatrFly <katrien.vliegen@gmail.com>
2021-10-27 18:04:05 +0200

    eraser toegevoegd en eerste poging voor de reset

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 const container = document.querySelector(".container");
-let n = prompt("hoeveel op hoeveel?");
+let n = 16;
 
-for (i = 0; i < n; i++) {
-    let div = document.createElement('div')
-    container.appendChild(div);
-    div.classList.add("rows");
+makeGrid(n);
+makeMagic(); 
 
-    for(j = 0; j < n; j++) {
-        let divv = document.createElement('div');
-        div.appendChild(divv);
-        divv.classList.add("grid");
-        console.log(i);
+const erase = document.querySelector('#eraser');
+erase.addEventListener("click", () => {
+    eraseColor();
+}); 
+
+function makeGrid(n) {
+    // n = prompt("hoeveel op hoeveel?");
+    for (i = 0; i < n; i++) {
+        let div = document.createElement('div')
+        container.appendChild(div);
+        div.classList.add("rows");
+
+        for(j = 0; j < n; j++) {
+            let divv = document.createElement('div');
+            div.appendChild(divv);
+            divv.classList.add("grid");
+            console.log(i);
+        }
     }
-
 }
 
-const divs = document.querySelectorAll('.grid');
 
-divs.forEach((div) => {
-    div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "purple";
+function makeMagic() {
+    const divs = document.querySelectorAll('.grid');
+
+    divs.forEach((div) => {
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = "pink";
+        });
     });
-});
+}
 
-const reset = document.querySelector('.reset');
-button.addEventListener("click", () => {
+
+
+const reset = document.querySelector('#reset');
+reset.addEventListener("click", () => {
     makeGrid(n);
+    makeMagic();
 })
 
-//een maximum groote op de hele container zetten, waarbinnen de grid altijd verdeeld wordt.
-//borders opzetten zodat je ze ziet, en een kleine margin tussen de divs.
-// eventlisteners opzetten voor wanneer er over een grid wordt gehovered. Dat geldt voor alle divs. 
+
+function eraseColor() {
+    const divs = document.querySelectorAll('.grid');
+
+    divs.forEach((div) => {
+        div.addEventListener('mouseenter', () => {
+            div.style.backgroundColor = "white";
+        });
+    });
+}
